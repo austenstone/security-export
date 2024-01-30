26f382294568e7def1a8da2de4bc1f8a335f9de2
Austen Stone <austenstone@github.com>
2024-01-30 12:38:04 -0500

    Add token and enterprise variables to usage.yaml workflow
    This commit adds the `token` and `enterprise` variables to the `usage.yaml` workflow. The `token` variable is set to the value of the `secrets.PAT` secret, and the `enterprise` variable is set to the value of the `vars.enterprise` variable. This allows the workflow to access the necessary credentials for authentication.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { getOctokit } from "@actions/github";
 
 interface Input {
   token: string;
+  organization?: string;
+  enterprise?: string;
+  repository?: string;
 }
 
 export function getInputs(): Input {
@@ -11,18 +14,68 @@ export function getInputs(): Input {
   if (!result.token || result.token === "") {
     throw new Error("github-token is required");
   }
+  result.repository = getInput("repository");
+  result.organization = getInput("organization");
+  result.enterprise = getInput("enterprise");
   return result;
 }
 
+
 const run = async (): Promise<void> => {
   const input = getInputs();
-  const octokit = getOctokit(input.token);
+  const octokit = getOctokit(input.token, {
+    throttle: {
+      onRateLimit: (retryAfter, options, octokit, retryCount) => {
+        octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`,);
 
-  const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
+        if (retryCount < 1) {
+          octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+        return false;
+      },
+      onSecondaryRateLimit: (_, options, octokit) => {
+        octokit.log.warn(
+          `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+        );
+      },
+    },
+  });
 
-  info(`Hello, ${login}!`);
+  const dependabotAlerts = await octokit.paginate({
+    method: 'GET',
+    url: input.organization ? `/orgs/${input.organization}/dependabot/alerts` : `/enterprises/${input.enterprise}/dependabot/alerts`,
+  });
+
+  console.log(dependabotAlerts);
+
+  const orgs = input.organization ? [input.organization] : await octokit.paginate({
+    method: 'GET',
+    url: '/user/orgs',
+  });
+
+  const repos = orgs.map(async (org) => {
+    return await octokit.paginate({
+      method: 'GET',
+      url: `/orgs/${org}/repos`,
+    });
+  }).flat();
+  
+  const codeScanningAlerts = repos.map(async (repo) => {
+    return await octokit.paginate({
+      method: 'GET',
+      url: `/repos/${repo.full_name}/code-scanning/alerts`,
+    });
+  });
+
+  console.log(codeScanningAlerts);
+
+  const secretScanningAlerts = await octokit.paginate({
+    method: 'GET',
+    url: input.organization ? `/orgs/${input.organization}/secret-scanning/alerts` : `/enterprises/${input.enterprise}/secret-scanning/alerts`,
+  });
+
+  console.log(secretScanningAlerts);
 };
 
 run();
